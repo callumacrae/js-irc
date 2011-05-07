8d1737d8b97f39b48167067433b0ae6bd7f61081
Callum Macrae <callumacrae95@gmail.com>
2011-05-07 14:31:07 +0100

    Added ability for multiple connections.

--- client.js
+++ client.js
@@ -1,50 +1,12 @@
 var sys = require('sys'),
 	options = require('./config'),
 	IRC = require('./lib/lib'),
-	irc = new IRC(options),
 	url = require('url'),
 	querystring = require('querystring'),
 	io = require('socket.io'),
 	http = require('http'),  
 	path = require('path'),
 	fs = require('fs');
-
-function handle_msg(data)
-{
-	if (data.msg == undefined || data.chan == undefined)
-	{
-		return false;
-	}
-
-	if (data.msg.slice(0, 1) == '/' && data.msg.slice(0, 2) != '//')
-	{
-		var command = data.msg.slice(1, data.msg.indexOf(' '));
-		var rest_of = data.msg.slice(data.msg.indexOf(' ') + 1, data.msg.length);
-		switch (command)
-		{
-			case "msg":
-			case "query":
-				var nick = rest_of.slice(0, rest_of.indexOf(' '));
-				var msg = rest_of.slice(rest_of.indexOf(' ') + 1, rest_of.length);
-				irc.raw('PRIVMSG ' + nick + ' :' + msg);
-				break;
-				
-			case "q":
-			case "quit":
-				irc.quit('https://github.com/callumacrae/irc-js/');
-				break;
-			
-			default:
-				irc.raw(command.toUpperCase() + ' ' + rest_of);
-				break;
-		}
-	}
-	else
-	{
-		irc.privmsg(data.chan, data.msg);
-	}
-	return true;
-}
 
 server = http.createServer(function (req, res)
 {
@@ -83,11 +45,47 @@ server.listen(1337, "127.0.0.1");
 var socket = io.listen(server);
 socket.on('connection', function(client)
 {
-	console.client = client; //horrible global!
-	irc.connect();
-	client.on('message', handle_msg);
+	client.irc = new IRC(options, client);
+	client.irc.connect();
+	client.on('message', function(data)
+	{
+		if (data.msg == undefined || data.chan == undefined)
+		{
+			return false;
+		}
+	
+		if (data.msg.slice(0, 1) == '/' && data.msg.slice(0, 2) != '//')
+		{
+			var command = data.msg.slice(1, data.msg.indexOf(' '));
+			var rest_of = data.msg.slice(data.msg.indexOf(' ') + 1, data.msg.length);
+			switch (command)
+			{
+				case "msg":
+				case "query":
+					var nick = rest_of.slice(0, rest_of.indexOf(' '));
+					var msg = rest_of.slice(rest_of.indexOf(' ') + 1, rest_of.length);
+					client.irc.raw('PRIVMSG ' + nick + ' :' + msg);
+					break;
+					
+				case "q":
+				case "quit":
+					client.irc.quit('https://github.com/callumacrae/irc-js/');
+					break;
+				
+				default:
+					client.irc.raw(command.toUpperCase() + ' ' + rest_of);
+					break;
+			}
+		}
+		else
+		{
+			client.irc.privmsg(data.chan, data.msg);
+		}
+		return true;
+	});
+
 	client.on('disconnect', function()
 	{
-		irc.quit('https://github.com/callumacrae/irc-js/');
-	}) 
+		client.irc.quit('https://github.com/callumacrae/irc-js/');
+	});
 });
